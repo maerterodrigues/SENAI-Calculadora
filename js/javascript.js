1ccffda0910c3e6dac15bee4b650f4c0917d9648
maerterodrigues <maerterodrigues@gmail.com>
2022-10-09 12:15:05 -0300

    adding function to calculate and display the result

--- js/javascript.js
+++ js/javascript.js
@@ -1,32 +1,43 @@
-var allClear = document.querySelector("#ac")
-var backspace = document.querySelector("#del")
-var numbers = document.querySelectorAll(".calculatorNumbers")
-var mathSymbols = document.querySelectorAll(".mathSymbols")
-var inputNumber = document.querySelector(".inputDisplay")
 
 
 
-for (var i = 0; i < numbers.length; i++) {
+(function calculus() {
+    var allClear = document.querySelector("#ac")
+    var backspace = document.querySelector("#del")
+    var numbers = document.querySelectorAll(".calculatorNumbers")
+    var mathSymbols = document.querySelectorAll(".mathSymbols")
+    var mathEqual = document.querySelector(".mathEqual")
+    var display = document.querySelector(".inputDisplay")
+
+    for (var i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", function() {
         var numbers = this.value
         parseFloat(numbers)
-        inputNumber.value += parseFloat(numbers)
+        display.value += parseFloat(numbers)
         console.log(parseFloat(numbers))
-        operation(numbers)
     })
 }
 
-for (var i = 0; i < mathSymbols.length; i++) {
+    for (var i = 0; i < mathSymbols.length; i++) {
     mathSymbols[i].addEventListener("click", function() {
         var symbol = this.value
-        inputNumber.value += symbol
+        display.value += symbol
         console.log(symbol)
-        operation(symbol)
     })
 }
 
+    mathEqual.addEventListener("click", function(){
+        if (display.value === "") {
+            display.value = ""
+        } else {
+            var result = eval(display.value)
+            display.value = result
+        }
+    })
 
-allClear.addEventListener("click", () => {
-    inputNumber.value = ""
-})
 
+    allClear.addEventListener("click", () => {
+        display.value = ""
+    })
+
+})()
